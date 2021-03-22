57a6a9ff75b152c48e45f5a58da569a99b07c714
piotr <piotrpopis98@icloud.com>
2021-03-22 21:59:43 +0100

    hasWin checker func

--- list02/exercise1/app/src/main/java/com/app/tictactoe/Board3x3Activity.kt
+++ list02/exercise1/app/src/main/java/com/app/tictactoe/Board3x3Activity.kt
@@ -2,6 +2,7 @@ package com.app.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -13,16 +14,16 @@ class Board3x3Activity : AppCompatActivity() {
     lateinit var player2ScoreTextView: TextView
 
     private val size = 3
-    private var winSequences: Array<Array<IntArray>> =
-        arrayOf(
-            arrayOf(intArrayOf(0, 0), intArrayOf(1, 1), intArrayOf(2, 2)),
-            arrayOf(intArrayOf(0, 2), intArrayOf(1, 1), intArrayOf(2, 0)),
-            arrayOf(intArrayOf(0, 0), intArrayOf(0, 1), intArrayOf(0, 2)),
-            arrayOf(intArrayOf(1, 0), intArrayOf(1, 1), intArrayOf(1, 2)),
-            arrayOf(intArrayOf(2, 0), intArrayOf(2, 1), intArrayOf(2, 2)),
-            arrayOf(intArrayOf(0, 0), intArrayOf(1, 0), intArrayOf(2, 0)),
-            arrayOf(intArrayOf(0, 1), intArrayOf(1, 1), intArrayOf(2, 1)),
-            arrayOf(intArrayOf(0, 2), intArrayOf(1, 2), intArrayOf(2, 2)),
+    private var winSequences: Set<Set<IntArray>> =
+        setOf(
+            setOf(intArrayOf(0, 0), intArrayOf(1, 1), intArrayOf(2, 2)),
+            setOf(intArrayOf(0, 2), intArrayOf(1, 1), intArrayOf(2, 0)),
+            setOf(intArrayOf(0, 0), intArrayOf(0, 1), intArrayOf(0, 2)),
+            setOf(intArrayOf(1, 0), intArrayOf(1, 1), intArrayOf(1, 2)),
+            setOf(intArrayOf(2, 0), intArrayOf(2, 1), intArrayOf(2, 2)),
+            setOf(intArrayOf(0, 0), intArrayOf(1, 0), intArrayOf(2, 0)),
+            setOf(intArrayOf(0, 1), intArrayOf(1, 1), intArrayOf(2, 1)),
+            setOf(intArrayOf(0, 2), intArrayOf(1, 2), intArrayOf(2, 2)),
         )
     private var player1Turn: Boolean = true
     private var roundCounter: Int = 0
@@ -31,8 +32,9 @@ class Board3x3Activity : AppCompatActivity() {
 
     private var player1Symbol: String = "X"
     private var player2Symbol: String = "O"
-    private var player1Fields: MutableList<Array<Int>> = mutableListOf()
-    private var player2Fields: MutableList<Array<Int>> = mutableListOf()
+
+    private var player1Fields: MutableSet<IntArray> = mutableSetOf()
+    private var player2Fields: MutableSet<IntArray> = mutableSetOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,8 +55,8 @@ class Board3x3Activity : AppCompatActivity() {
         val buttonReset: Button = findViewById(R.id.resetButton)
         buttonReset.setOnClickListener {
             Toast.makeText(applicationContext, "Game reset!", Toast.LENGTH_SHORT).show()
-            player1Fields = mutableListOf()
-            player2Fields = mutableListOf()
+            player1Fields = mutableSetOf()
+            player2Fields = mutableSetOf()
             player1Turn = true
             roundCounter = 0
             player1Score = 0
@@ -78,27 +80,60 @@ class Board3x3Activity : AppCompatActivity() {
             if (player1Turn) {
                 button.text = player1Symbol
                 player1Turn = false
-                player1Fields.add(arrayOf(r, c))
+                player1Fields.add(intArrayOf(r, c))
+
+                Toast.makeText(
+                    applicationContext,
+                    hasWin(player1Fields).toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
             } else {
                 button.text = player2Symbol
                 player1Turn = true
-                player1Fields.add(arrayOf(r, c))
+                player2Fields.add(intArrayOf(r, c))
+
+                Toast.makeText(
+                    applicationContext,
+                    hasWin(player2Fields).toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
+        } else {
+            Toast.makeText(applicationContext, "Field is busy.", Toast.LENGTH_SHORT).show()
+
         }
-        Toast.makeText(applicationContext, player2Fields.toString(), Toast.LENGTH_SHORT)
-            .show()
     }
 
-    private fun hasWin(fields: MutableList<Array<Int>>):Boolean {
-        if (fields.size>2){
-        for (seq in winSequences){
-            fields.containsAll(seq)
-        }
+    private fun hasWin(fields: MutableSet<IntArray>): Boolean {
+        // I really can not find how to compare two Sets of Arrays in Kotlin.
 
+        if (fields.size > 2) {
+            Log.i("info", "++++++++++++++++++FIELDS+++++++++++++++++++++++++")
+            Log.i("info", fields.javaClass.name)
+            for (seq in winSequences) {
+
+                Log.i("info", "----------------------------------------")
+                var counter = 0;
+                for (cords in seq) {
+                    Log.i("info", cords.contentToString())
+                    for (f in fields) {
+
+                        Log.i("info", f.contentToString())
+
+                        if (cords.contentToString() == f.contentToString()) {
+                            counter++
+                            Log.i("info", counter.toString())
+                        }
+                        if (counter == size) {
+                            return true
+                        }
+                    }
+                }
+
+            }
         }
         return false
     }
-
 
     private fun isFieldBusy(button: Button): Boolean {
         if (button.text == "O" || button.text == "X") {
